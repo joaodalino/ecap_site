6b8b48ae5e65b601651e0f33f8626d0b6a224353
Mikael Adami <mikaeladami2018@gmail.com>
2024-09-07 18:48:09 -0300

    Estrutura

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>e-cap</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <main>
+        <header>
+            <img src="imagens/logoecap" alt="Logo E-cap">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="#">Patrocinadores</a></li>
+                <li><a href="">Contato</a></li>
+                <li><a href="">Veículos</a></li>
+                <li><a href="">Faça Parte</a></li>
+            </ul>
+        </header>
+        <nav>
+            <h3></h3>
+            <a href="#about">Sobre nós</a>
+        </nav>
+    </main>
+    <div class="destaques">
+
+    </div>
+    <div class="sobre" id="about">
+
+    </div>
+    <div class="parceiros" id="patrocinadores">
+
+    </div>
+    <div class="capacidades">
+
+    </div>
+    <footer>
+        <div class="rodape">
+            <nav class="redes">
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a>
+            </nav>
+            <ul class="rodapas">
+                <li><a href="">Home</a></li>
+                <li><a href="">Patrocinadores</a></li>
+                <li><a href="">Contato</a></li>
+                <li><a href="">Veículos</a></li>
+                <li><a href="">Sobre</a></li>
+            </ul>
+        </div>
+    </footer>
+    <script src="script.js"></script>
+</body>
+</html>
